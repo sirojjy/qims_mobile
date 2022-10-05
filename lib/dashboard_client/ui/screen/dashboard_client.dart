1a0476071e96c897742bc70113161f436ea31b32
sirojahmad <siroj.bi@gmail.com>
2022-10-05 23:05:30 +0700

    PC: add dashboard admin 1 container sertifikat, profil 2

--- lib/dashboard_client/ui/screen/dashboard_client.dart
+++ lib/dashboard_client/ui/screen/dashboard_client.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qims_mobile/share/custom_container.dart';
+import 'package:timelines/timelines.dart';
 
 class DashboardClient extends StatefulWidget {
   const DashboardClient({Key? key}) : super(key: key);
@@ -14,97 +15,113 @@ class _DashboardClientState extends State<DashboardClient> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Dashboard Admin1'),
+          leading: Padding(
+            padding: EdgeInsets.all(10),
+            child: ClipRRect(
+              child: Image.asset(
+                'assets/images/logo_qai.jpg',
+                scale: 1,
+                // height: 10,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          title: Text('Dashboard User'),
+          actions: [
+            Container(
+              width: 50,
+              child: Icon(Icons.person),
+            )
+          ],
         ),
-        body: Container(
-          padding: EdgeInsets.only(left: 10, right: 10),
-          child:
-          Column(
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  CustomContainer(
-                    color: Colors.blue,
-                    icon: Icon(Icons.pageview),
-                    text1: '1',
-                    text2: 'Sertifikat Aktif',
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  CustomContainer(
-                    color: Colors.blue,
-                    icon: Icon(Icons.pageview),
-                    text1: '1',
-                    text2: 'Sertifikat Aktif',
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  CustomContainer(
-                    color: Colors.blue,
-                    icon: Icon(Icons.pageview),
-                    text1: '1',
-                    text2: 'Sertifikat Aktif',
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  CustomContainer(
-                    color: Colors.blue,
-                    icon: Icon(Icons.pageview),
-                    text1: '1',
-                    text2: 'Sertifikat Aktif',
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                decoration: BoxDecoration(
-                    color: Colors.blue[100],
-                    borderRadius: BorderRadius.circular(5)),
-                child:
-                Column(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child:
+            Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Expanded(child:
-                          Text('Sertifikat',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.black87,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            )
+                    CustomContainer(
+                      color: Colors.blue,
+                      icon: Icons.pageview,
+                      text1: '1',
+                      text2: 'Sertifikat Aktif',
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    CustomContainer(
+                      color: Colors.blue,
+                      icon: Icons.pageview,
+                      text1: '1',
+                      text2: 'Sertifikat Aktif',
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    CustomContainer(
+                      color: Colors.blue,
+                      icon: Icons.pageview,
+                      text1: '1',
+                      text2: 'Sertifikat Aktif',
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    CustomContainer(
+                      color: Colors.blue,
+                      icon: Icons.pageview,
+                      text1: '1',
+                      text2: 'Sertifikat Aktif',
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      borderRadius: BorderRadius.circular(5)),
+                  child:
+                  Column(
+                    children: [
+                      Row(
+                          children: [
+                            Expanded(child:
+                            Text('Sertifikat',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                )
+                            ),
+                            ),
+                          ]),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ClipRRect(
+                            child: Image.asset(
+                              'assets/images/9001.png',
+                              height: 60,
+                            ),
+                            borderRadius: BorderRadius.circular(5),
                           ),
-                        ),
-                    ]),
-                    Row(
-                      children: [
-                        Column(children: [
-                          SizedBox(height: 10,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ClipRRect(
-                                child: Image.asset(
-                                  'assets/images/9001.png',
-                                  height: 60,
-                                ),
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              SizedBox(width: 20,),
-                              Column(
+                          SizedBox(width: 20,),
+                          Expanded(
+                              child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text('PPS Cilacap',
@@ -115,106 +132,152 @@ class _DashboardClientState extends State<DashboardClient> {
                                       )),
                                   SizedBox(height: 5,),
                                   SizedBox(
-                                      width: 120,
-                                      child: Text("ISO 9001:2015"),
-                                    ),
+                                    width: 120,
+                                    child: Text("ISO 9001:2015"),
+                                  ),
                                   SizedBox(
-                                      height: 5,
-                                    ),
-                                    InkWell(
-                                      child: Container(
-                                        width: 150,
-                                        height: 35,
-                                        padding: EdgeInsets.all(5),
-                                        decoration: BoxDecoration(
-                                            color: Colors.blue,
-                                            borderRadius: BorderRadius.circular(5)),
-                                        child: Center(
-                                          child: Text(
-                                            'Lihat Sertifikat',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w400,
-                                              fontSize: 14,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                )
+                                    height: 5,
+                                  ),
+                                  Container(
+                                    child:Text('Scope : Provision of Management and Administration of Fish Diseases, Quality and Safety of Fishery Product'),
+                                  )
+                                ],
+                              )
+
+                          )
+                        ],
+                      ),
+
+                      SizedBox(height: 10,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            child: Container(
+                              width: 150,
+                              height: 35,
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(25)),
+                              child: Center(
+                                child: Text(
+                                  'Lihat Sertifikat',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                SizedBox(
+                  height: 10,
+                ),
+
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      borderRadius: BorderRadius.circular(5)),
+                  child:
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(child:
+                          Text('Profil',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )
+                          ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Nama'),
+                              Text('Alamat'),
+                              Text('PIC'),
+                              Text('No. HP'),
+                            ],
+                          ),
+                          SizedBox(width: 10,),
+                          Column(
+                            children: [
+                              Text(':'),
+                              Text(':'),
+                              Text(':'),
+                              Text(':'),
+                            ],
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('PPS Cilacap'),
+                                Text('Jl. Penyu No. 12, Cilacap, Jawa Tengah'),
+                                Text('Bu Eko'),
+                                Text('08128xxxxxxx'),
                               ],
                             ),
-                          ]
-                        ),
-                      ],
-                    )
-                  ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
+                SizedBox(height: 10,),
 
-              SizedBox(
-                height: 10,
-              ),
-
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                decoration: BoxDecoration(
-                    color: Colors.blue[100],
-                    borderRadius: BorderRadius.circular(5)),
-                child:
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(child:
-                            Text('Profil',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.black87,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            )
-                            ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10,),
-
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text('Column 1'),
-                            Text('Column 1'),
-                            Text('Column 1'),
-                            Text('Column 1'),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('Column 2'),
-                            Text('Column 2'),
-                            Text('Column 2'),
-                            Text('Column 2'),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text('Column 3'),
-                            Text('Column 3'),
-                            Text('Column 3'),
-                            Text('Column 3'),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
+                Container(
+                  // alignment: Alignment.centerLeft,
+                    height: 900,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.blue[100],
+                        borderRadius: BorderRadius.circular(5)),
+                   child:
+                   FixedTimeline.tileBuilder(
+                     builder: TimelineTileBuilder.connectedFromStyle(
+                       connectionDirection: ConnectionDirection.before,
+                       contentsAlign: ContentsAlign.reverse,
+                       oppositeContentsBuilder: (context, index) => Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                             Text('09-07-2022', style: TextStyle(fontWeight: FontWeight.bold),),
+                             Text('Sertifikasi Awal', style: TextStyle(fontWeight: FontWeight.bold),),
+                             Text('Scope : Provision of Management and Administration of Fish Diseases, Quality and Safety of Fishery Product'),
+                           ],
+                         ),
+                       ),
+                       connectorStyleBuilder: (context, index) => ConnectorStyle.solidLine,
+                       indicatorStyleBuilder: (context, index) => IndicatorStyle.dot,
+                       itemCount: 3,
+                     ),
+                   )
+                  ,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        )
       ),
     );
   }
--- lib/share/custom_container.dart
+++ lib/share/custom_container.dart
@@ -1,33 +1,74 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class CustomContainer extends StatelessWidget {
-  final Icon icon;
+  // final Icons icon;
+  final IconData? icon;
   final String? text1;
   final String? text2;
   final Color? color;
-  const CustomContainer({Key? key, required this.icon, this.text1, this.text2, this.color}) : super(key: key);
+  const CustomContainer({Key? key, this.text1, this.text2, this.color, required, this.icon }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
         child: Container(
-          padding: EdgeInsets.only(top: 20, bottom: 20),
+          // padding: EdgeInsets.only(top: 20, bottom: 20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(5),
             color: color,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              )
+            ],
           ),
-          child: Column(
+          child:
+          Column(
             children: [
-              icon,
-              Text(
-                '$text1',
-                style: TextStyle(fontSize: 30,
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(5),topRight: Radius.circular(5)),
+                    color: Colors.green,
+                  ),
+                  width: double.infinity,
+                  height: 70,
+                  child:
+                  Icon(icon, color: Colors.white,size: 50,),
+                ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5),bottomRight: Radius.circular(5)),
                   color: Colors.white,
                 ),
-              ),
-              Text('$text2',
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
+                height: 40,
+                child:
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '$text1',
+                      style: TextStyle(fontSize: 14,
+                        // color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Text('$text2',
+                      style: TextStyle(
+                          fontSize: 14,
+                          // color: Colors.white
+                      ),
+                    ),
+                  ],
+                )
+                ,
+              )
+
             ],
           ),
         )
